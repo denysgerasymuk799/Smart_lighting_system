55a58eb84062a4aaee1dfb9305560dda03517d7d
JanusDG <d.bilusyak@gmail.com>
2021-01-16 15:35:37 +0200

    added clap

---
+++ Mesh_Flood_PSoC4BLE_Main_Clap.cydsn/customize.h
@@ -0,0 +1,16 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+
+#include "project.h"
+int get_compare_from_analog(uint16 val, uint16 init_val, uint16 prev_val);
