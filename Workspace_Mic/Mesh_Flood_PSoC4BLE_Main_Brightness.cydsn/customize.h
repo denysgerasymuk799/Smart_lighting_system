f181a57ec5c8db5badf0973a8482ae14a1cecea7
JanusDG <d.bilusyak@gmail.com>
2021-01-17 05:41:36 +0200

    added mic

---
+++ Workspace_Mic/Mesh_Flood_PSoC4BLE_Main_Brightness.cydsn/customize.h
@@ -0,0 +1,17 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+
+#include "project.h"
+int get_compare_from_analog(uint16 val, uint16 init_val, uint16 prev_val);
+int get_brightness_from_analog(uint16 val, uint16 init_val, uint16 prev_val);
